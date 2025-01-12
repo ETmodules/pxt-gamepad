ed5255f42165ff0ba33102b7df2e707640b256f6
ETmodules <etmodules@outlook.com>
2025-01-12 16:25:33 +0100

    main.ts bijwerken

--- main.ts
+++ main.ts
@@ -138,7 +138,7 @@ namespace EtGamepad {
             case Button.Button8: EventGamepad8 = programmableCode; break;
             case Button.Button9: EventGamepad9 = programmableCode; break;
             case Button.Button10: EventGamepad10 = programmableCode; break;
-            case Button.Button11: EventGamepad11 = programmableCode; break;
+            case Button.Button11: EventGamepad11 = programmableCode; basic.showNumber(button); break;
             case Button.Button12: EventGamepad12 = programmableCode; break;
         }
     }
@@ -180,18 +180,18 @@ namespace EtGamepad {
         radio.sendNumber(button)
 
         switch (button) {
-            case Button.Button1: if (EventGamepad1) basic.showNumber(button); EventGamepad1; break;
-            case Button.Button2: if (EventGamepad2) basic.showNumber(button); EventGamepad2; break;
-            case Button.Button3: if (EventGamepad3) basic.showNumber(button); EventGamepad3; break;
-            case Button.Button4: if (EventGamepad4) basic.showNumber(button); EventGamepad4; break;
-            case Button.Button5: if (EventGamepad5) basic.showNumber(button); EventGamepad5; break;
-            case Button.Button6: if (EventGamepad6) basic.showNumber(button); EventGamepad6; break;
-            case Button.Button7: if (EventGamepad7) basic.showNumber(button); EventGamepad7; break;
-            case Button.Button8: if (EventGamepad8) basic.showNumber(button); EventGamepad8; break;
-            case Button.Button9: if (EventGamepad9) basic.showNumber(button); EventGamepad9; break;
-            case Button.Button10: if (EventGamepad10) basic.showNumber(button); EventGamepad10; break;
-            case Button.Button11: if (EventGamepad11) basic.showNumber(button); EventGamepad11; break;
-            case Button.Button12: if (EventGamepad12) basic.showNumber(button); EventGamepad12; break;
+            case Button.Button1: if (EventGamepad1) EventGamepad1; break;
+            case Button.Button2: if (EventGamepad2) EventGamepad2; break;
+            case Button.Button3: if (EventGamepad3) EventGamepad3; break;
+            case Button.Button4: if (EventGamepad4) EventGamepad4; break;
+            case Button.Button5: if (EventGamepad5) EventGamepad5; break;
+            case Button.Button6: if (EventGamepad6) EventGamepad6; break;
+            case Button.Button7: if (EventGamepad7) EventGamepad7; break;
+            case Button.Button8: if (EventGamepad8) EventGamepad8; break;
+            case Button.Button9: if (EventGamepad9) EventGamepad9; break;
+            case Button.Button10: if (EventGamepad10) EventGamepad10; break;
+            case Button.Button11: if (EventGamepad11) EventGamepad11; break;
+            case Button.Button12: if (EventGamepad12) EventGamepad12; break;
         }
     }
 
